494a10bc228563721434a5cbccd87d2ff446cdb4
L. Avezum <u1273941@campus.uvt.nl>
2019-07-29 13:42:39 +0200

    Add code that import raw datasets (orbis, pillar 3 and auxiliar tables) and deleted old Import file

--- Data/Import.R
+++ Data/Import.R
@@ -15,9 +15,8 @@ library(data.table)   ## Command to bind lists
 library(reshape2)     ## Command to transform data from wide to long
 library(stringr)
 
-# Load auxiliar data frame: list of parent banks
-guo.list <- read_xlsx("../../../Data/Financial/Exposure_data_collection/Auxiliar/List.xlsx",sheet = 1) %>%
-  select(name,bvdid)
+# Load auxiliar data frame: list of parent banks with all BvDid's and IRB adoption year
+IRB.indicator <- read_xlsx("Data/Raw/Pillar3/SA_banks/SA_banks.xlsx",sheet = 1) 
 
 ##============================================================================##
 ## Pillar-III reports                                                         ##
@@ -32,7 +31,9 @@ pillar3.data <-  rbindlist(data.list, use.names=TRUE,fill = TRUE) %>%
   select(1:25)
 
 # Add short names to data frame
-pillar3.data <- inner_join(pillar3.data, guo.list)
+pillar3.data <- IRB.indicator %>%
+  select(name, bvdid_new) %>% rename(bvdid = bvdid_new) %>%
+  inner_join(pillar3.data)
 
 # Save data frame
 save(pillar3.data,file=paste0("Data/Temp/Pillar3Data.Rda"))
@@ -41,51 +42,67 @@ save(pillar3.data,file=paste0("Data/Temp/Pillar3Data.Rda"))
 ## Orbis Bank Focus (BankScope)                                               ##
 ##============================================================================##
 
-# Import Orbis dataset from txt file 
+# Import Orbis dataset from excel files 
 data.path  <- "Data/Raw/BankScope"
-data.names <-  list.files(path = data.path,pattern=".txt",full.names = TRUE)
-data.list  <- data.names %>% lapply(read.delim) 
-
-# Create variable year and adjust by month (change to previous year if June of before),
-# keep consolidate information
+data.names <-  list.files(path = data.path,pattern=".*(201.)\\.xlsx",full.names = TRUE)
+data.list  <- data.names %>%
+  lapply(read_xlsx,sheet = 1) 
 
 for(i in 1:length(data.list)) {
-data.list[[i]] <- data.list[[i]] %>%
-  mutate(month=as.numeric(sapply(Closing.date,str_sub,start= 5,end=6)),
-         year=as.numeric(sapply(Closing.date,str_sub,start= 1,end=4)),
-         sample.version = i) %>%
-  mutate(year=ifelse(is.na(month),year,
-                     ifelse(month<7,year-1,year))) %>% 
-  rename(bvdid=colnames(data.list[[i]])[1]) %>%
-  filter(Consolidation.code %in% c("C1","C2"))
-}
+  data.list[[i]] <- data.list[[i]] %>%
+    # Remove row index, set variables names to lower case, rename id variable, and keep only selected sample
+    select(-contains("_"))  %>%
+    rename_all(tolower)%>%
+    rename_at(vars(contains("bvd")), 
+              funs(str_replace(.,"bvd .*", "bvdid")))%>%
+    # Standardize variables names to reshape dataset from wide to long
+    # The first two lines  code remove several caracteres, the second add a dot before the year indicator
+    rename_at(vars(contains("last")), 
+              funs(str_replace(.,"last .*", "10")))%>%
+  rename_all(funs(str_replace_all(.,"[[:space:]]|[\\(*\\),]|\\[|\\]|\\%|\\/|\\.|\\=|\\-|year","")))%>%
+  rename_all(funs(str_replace_all(.,"([\\w])([0-9]+)$", "\\1\\.\\2")))
 
-# Bind old (2007-2016) and new dataset (2008-2018) 
-bankscope <-  rbindlist(data.list, use.names=TRUE,fill = TRUE) %>%
-  distinct(bvdid,year, .keep_all = TRUE) 
+  # Reshape datasets to long format
+  data.list[[i]] <- reshape(data.list[[i]],
+                            varying   = c(grep("10", names(data.list[[i]]))[1]:ncol(data.list[[i]])),
+                            direction = 'long', 
+                            timevar   = 'year')
+  names(data.list[[i]]) <- names(data.list[[1]])
+  }
+
+# Bind datasets from different disks  
+bankscope <-  rbindlist(data.list, use.names=TRUE,fill = TRUE)
+
+# Standardize bvdid and add IRB adoption variable
+IRB.indicator.long <- IRB.indicator %>%
+  rename_if(is.numeric, function(x) paste("IRB", x, sep = ".")) %>%
+  reshape(varying   = c(grep("[0-9]", names(.))),
+          direction = 'long', 
+          timevar   ='year')%>%
+  select(name, Country, bvdid_new, bvdid_old, year, IRB)
+
+bankscope <- bankscope %>% 
+  mutate(month = as.numeric(sapply(closingdate,str_sub,start= 6,end=7)),
+         year  = as.numeric(sapply(closingdate,str_sub,start= 1,end=4)),
+         year  = ifelse(is.na(month),year,ifelse(month<6,year-1,year))) %>%
+  full_join(IRB.indicator.long, by = c("bvdid" = "bvdid_old", "year")) %>%
+  full_join(IRB.indicator.long, by = c("bvdid" = "bvdid_new", "year")) %>%
+    mutate(bvdid = ifelse(is.na(bvdid_old), 
+                          ifelse(is.na(bvdid_new), bvdid, bvdid_new), 
+                          bvdid)) %>%
+  # Keep unique bank-year observations
+  filter(conscode %in% c("C1","C2","U1")) %>%
+  distinct(bvdid,year, .keep_all = TRUE) %>%
+  mutate(IRB = ifelse(is.na(IRB.x),IRB.y,IRB.x)) %>%
+  filter(bvdid %in% IRB.indicator$bvdid_new) %>%
+  # Remove auxiliar variables
+  select(-contains("_")) %>% select(-contains(".")) %>%
+  select(-c("companyname", "countryisocode","lastavail", "id")) 
+  # Turn numeric variables to numeric format
+  bankscope <- bankscope %>% 
+  mutate_at(c(grep("usd", names(bankscope))[1]:ncol(bankscope)),as.numeric)
 
 # Save data frame
 save(bankscope,file=paste0("Data/Temp/BankScope.Rda"))
-
-
-
-bankscope <- read_xlsx("Data/Raw/BankScope/BankFocus_Export.xlsx",sheet = 2)
-bankscope <- select(bankscope,-contains("_"))
-threshold <- which(colnames(bankscope)%in%grep("2018", names(bankscope), value=TRUE))[1]
-names(bankscope) <- gsub("[[:space:]]", "", names(bankscope))
-names(bankscope) <- gsub("[\\(*\\),]", "",names(bankscope))
-names(bankscope) <- gsub("[\\%,]", "",names(bankscope))
-names(bankscope) <- gsub("[\\/,]", "",names(bankscope))
-names(bankscope) <- gsub("[\\.,]", "",names(bankscope))
-names(bankscope) <- gsub("(*)([0-9]+)", "\\1\\.\\2", names(bankscope))
-names(bankscope) <- gsub("(\\.)(1)", "\\2", names(bankscope))
-
-test <-reshape(bankscope, 
-               varying   = c(threshold:ncol(bankscope)),
-               direction = 'long', 
-               timevar   ='year')
-
-
-
 
 
