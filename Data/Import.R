7f2b27c01a0bf9b58daa3babcca306b96a9f4a76
L. Avezum <u1273941@campus.uvt.nl>
2019-08-10 15:27:17 +0200

    Add basel II and EBA exercise variables to Import.R

--- Data/Import.R
+++ Data/Import.R
@@ -13,10 +13,28 @@ library(tidyverse)    ## Data manipulation, pipe operator
 library(readxl)       ## Command to open xlsx files                                                     
 library(data.table)   ## Command to bind lists
 library(reshape2)     ## Command to transform data from wide to long
-library(stringr)
+library(stringr)      ## Command to clean variables names in Orbis
 
-# Load auxiliar data frame: list of parent banks with all BvDid's and IRB adoption year
-IRB.indicator <- read_xlsx("Data/Raw/Pillar3/SA_banks/SA_banks.xlsx",sheet = 1) 
+
+##============================================================================##
+## Auxiliar data                                                              ##
+##============================================================================##
+
+# IRB adoption year
+IRB.indicator <- read_xlsx("Data/Raw/Pillar3/Auxiliar/Auxiliar.xlsx",sheet = 1)  %>%
+  rename_if(is.numeric, function(x) paste("IRB", x, sep = ".")) %>%
+  reshape(varying   = c(grep("[0-9]", names(.))),
+          direction = 'long', 
+          timevar   ='year')%>%
+  select(name, Country, bvdid_new, bvdid_old, year, IRB)
+
+# Basel II introduction
+basel.indicator <- read_xlsx("Data/Raw/Pillar3/Auxiliar/Auxiliar.xlsx",sheet = 2) %>%
+  select(Country, basel = year)
+
+# EBA capital exercise
+EBA.indicator <- read_xlsx("Data/Raw/Pillar3/Auxiliar/Auxiliar.xlsx",sheet = 3) %>%
+  select(bvdid, EBAbank, EBAcountry, Shortfall.mn, Shortfall.RW)
 
 ##============================================================================##
 ## Pillar-III reports                                                         ##
@@ -32,7 +50,8 @@ pillar3.data <-  rbindlist(data.list, use.names=TRUE,fill = TRUE) %>%
 
 # Add short names to data frame
 pillar3.data <- IRB.indicator %>%
-  select(name, bvdid_new) %>% rename(bvdid = bvdid_new) %>%
+  select(name, bvdid = bvdid_new) %>% 
+  distinct(bvdid, .keep_all = TRUE) %>%
   inner_join(pillar3.data)
 
 # Save data frame
@@ -48,6 +67,10 @@ data.names <-  list.files(path = data.path,pattern=".*(201.)\\.xlsx",full.names 
 data.list  <- data.names %>%
   lapply(read_xlsx,sheet = 1) 
 
+#------------------------------------------------------------------------------#
+# Reshape data from wide to long                                               #
+#------------------------------------------------------------------------------#
+
 for(i in 1:length(data.list)) {
   data.list[[i]] <- data.list[[i]] %>%
     # Remove row index, set variables names to lower case, rename id variable, and keep only selected sample
@@ -56,12 +79,11 @@ for(i in 1:length(data.list)) {
     rename_at(vars(contains("bvd")), 
               funs(str_replace(.,"bvd .*", "bvdid")))%>%
     # Standardize variables names to reshape dataset from wide to long
-    # The first two lines  code remove several caracteres, the second add a dot before the year indicator
+    # The first two lines of code remove several caracteres, the second add a dot before the year indicator
     rename_at(vars(contains("last")), 
               funs(str_replace(.,"last .*", "10")))%>%
   rename_all(funs(str_replace_all(.,"[[:space:]]|[\\(*\\),]|\\[|\\]|\\%|\\/|\\.|\\=|\\-|year","")))%>%
   rename_all(funs(str_replace_all(.,"([\\w])([0-9]+)$", "\\1\\.\\2")))
-
   # Reshape datasets to long format
   data.list[[i]] <- reshape(data.list[[i]],
                             varying   = c(grep("10", names(data.list[[i]]))[1]:ncol(data.list[[i]])),
@@ -73,34 +95,35 @@ for(i in 1:length(data.list)) {
 # Bind datasets from different disks  
 bankscope <-  rbindlist(data.list, use.names=TRUE,fill = TRUE)
 
-# Standardize bvdid and add IRB adoption variable
-IRB.indicator.long <- IRB.indicator %>%
-  rename_if(is.numeric, function(x) paste("IRB", x, sep = ".")) %>%
-  reshape(varying   = c(grep("[0-9]", names(.))),
-          direction = 'long', 
-          timevar   ='year')%>%
-  select(name, Country, bvdid_new, bvdid_old, year, IRB)
+#------------------------------------------------------------------------------#
+# First layer of data cleaning                                                 #
+#------------------------------------------------------------------------------#
 
-bankscope <- bankscope %>% 
+bankscope <- bankscope %>%
+  # Create and correct year variable
   mutate(month = as.numeric(sapply(closingdate,str_sub,start= 6,end=7)),
          year  = as.numeric(sapply(closingdate,str_sub,start= 1,end=4)),
          year  = ifelse(is.na(month),year,ifelse(month<6,year-1,year))) %>%
-  full_join(IRB.indicator.long, by = c("bvdid" = "bvdid_old", "year")) %>%
-  full_join(IRB.indicator.long, by = c("bvdid" = "bvdid_new", "year")) %>%
-    mutate(bvdid = ifelse(is.na(bvdid_old), 
-                          ifelse(is.na(bvdid_new), bvdid, bvdid_new), 
-                          bvdid)) %>%
-  # Keep unique bank-year observations
-  filter(conscode %in% c("C1","C2","U1")) %>%
+  # Harmonize bvdid (for some banks and countries it has changed from one disk to another)
+  full_join(IRB.indicator, by = c("bvdid" = "bvdid_old", "year")) %>%
+  full_join(IRB.indicator, by = c("bvdid" = "bvdid_new", "year")) %>%
+  mutate(bvdid = ifelse(is.na(bvdid_old), ifelse(is.na(bvdid_new), bvdid, bvdid_new), bvdid)) %>%
+  # Keep unique bank-year observations from sample, and remove strange observations from 1969 (?!?)
+  filter(bvdid %in% IRB.indicator$bvdid_new, year != 1969, conscode %in% c("C1","C2","U1")) %>%
   distinct(bvdid,year, .keep_all = TRUE) %>%
-  mutate(IRB = ifelse(is.na(IRB.x),IRB.y,IRB.x)) %>%
-  filter(bvdid %in% IRB.indicator$bvdid_new) %>%
-  # Remove auxiliar variables
-  select(-contains("_")) %>% select(-contains(".")) %>%
-  select(-c("companyname", "countryisocode","lastavail", "id")) 
+  # Fix variables from the IRB indicator data frame
+  mutate(IRB     = ifelse(is.na(IRB.x),IRB.y,IRB.x),
+         name    = ifelse(is.na(name.x),name.y,name.x),
+         Country = ifelse(is.na(Country.x),Country.y,Country.x)) %>%
+  # Reorder variables and remove auxiliar variables
+  select(name,  Country, IRB, everything(), -contains("_"), -contains("."),
+         -c("companyname", "countryisocode","lastavail", "id")) %>% 
   # Turn numeric variables to numeric format
-  bankscope <- bankscope %>% 
-  mutate_at(c(grep("usd", names(bankscope))[1]:ncol(bankscope)),as.numeric)
+  mutate_at(c(grep("usd", names(bankscope))[1]:ncol(bankscope)),as.numeric) %>%
+  # Add Basel 2 country level introduction year
+  left_join(basel.indicator, by = c("Country"))
+  # Add EBA capital exercise information
+bankscope <- bankscope %>% left_join(EBA.indicator, by = c("bvdid"))
 
 # Save data frame
 save(bankscope,file=paste0("Data/Temp/BankScope.Rda"))
